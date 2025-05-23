ad41085a18fc8ad119346dd6d754e8812ebe2c7f
Divij Sinha <80167639+divij-sinha@users.noreply.github.com>
2025-05-23 12:44:06 -0500

    feat: enhance FastAPI routes with request handling and improve HTML styling with Tailwind CSS

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-
+from fastapi.staticfiles import StaticFiles
 from games import SchengenShowdown
 from models import Position
 
@@ -11,24 +11,25 @@ templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 schengen_showdown_s13 = SchengenShowdown("rules/schengenshowdowns13.json")
 print(schengen_showdown_s13.board)
 print("----")
 
 
 @app.get("/")
-def read_root():
-    return templates.TemplateResponse("index.html", {"request": {}, "board": schengen_showdown_s13.board})
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "board": schengen_showdown_s13.board})
 
 
 @app.get("/team_travel")
-def team_travel(team_name: str, new_pos: str, cost: int = 0):
+def team_travel(request: Request, team_name: str, new_pos: str, cost: int = 0):
     new_pos = Position(name=new_pos, type="country")
     messages = schengen_showdown_s13.team_travel(team_name, new_pos, cost)
-    return templates.TemplateResponse("index.html", {"request": {}, "board": schengen_showdown_s13.board, "messages": messages})
+    return templates.TemplateResponse("index.html", {"request": request, "board": schengen_showdown_s13.board, "messages": messages})
 
 
 @app.get("/pull_card")
-def pull_card(team_name: str):
+def pull_card(request: Request, team_name: str):
     messages = schengen_showdown_s13.pull_card("main_deck", team_name)
-    return templates.TemplateResponse("index.html", {"request": {}, "board": schengen_showdown_s13.board, "messages": messages})
+    return templates.TemplateResponse("index.html", {"request": request, "board": schengen_showdown_s13.board, "messages": messages})
